6b0ba54b63cf2b8d96022c2a8749a6de8ecf519c
Shane Hendrickson <shane.hendrickson@gmail.com>
2022-07-12 13:42:10 -0400

    Finished html css started js

---
+++ js/site.js
@@ -0,0 +1,25 @@
+// get the values from the Page
+// starts or controller function
+function getValues() {
+  // get values from the page
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
+
+  alert("Finding numbers from " + startValue + " to " + endValue + ".")
+
+  // call generateNumbers
+
+  // call displayNumbers
+}
+
+// generate numbers from startValue to endValue
+// logic functions
+function generateNumbers() {
+
+}
+
+// display the numbers and mark even numbers bold
+// display or view functions
+function displayNumbers() {
+
+}
